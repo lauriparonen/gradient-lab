dc28a5a3a1a52d123bf07b046b51e564ba14d2b2
Lauri Paronen <lauriparonen@outlook.com>
2025-06-28 11:50:31 +0300

    baseline implementation

---
+++ src/types/gl-react.d.ts
@@ -0,0 +1,9 @@
+declare module 'gl-react-dom' {
+    export const Surface: any;
+  }
+  
+  declare module 'gl-react' {
+    export const Node: any;
+    export const Shaders: any;
+    export const Uniform: any;
+  } 
